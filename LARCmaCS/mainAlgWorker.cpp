9f93ae4e8003577ba4dd9160d0febe93a632b2c3
anastasiia-kornilova <kornilova.anastasiia@gmail.com>
2019-04-29 15:34:26 +0300

    Unnecessary initConfig parser was removed

--- LARCmaCS/mainAlgWorker.cpp
+++ LARCmaCS/mainAlgWorker.cpp
@@ -1,75 +1,9 @@
 #include "mainAlgWorker.h"
 
-#include <QtWidgets/QApplication>
-#include <fstream>
-#include "QDebug"
+#include <QDebug>
+#include <QApplication>
+
 #include "message.h"
-
-int initConfig(RCConfig *config){
-	ifstream configFile;
-	string line;
-
-	configFile.open("LARCmaCS.cnf", ios::in);
-
-	if(!configFile.is_open()) {
-		cerr << "Can't open configuration file" << endl;
-		return -1;
-	}
-
-	if(!config) {
-		cerr << "Can't parse configuration file" << endl;
-		return -2;
-	}
-
-	while(!configFile.eof()) {
-		getline(configFile, line);
-
-		if (line.empty())
-			continue;
-
-		if(line.find("rfclient.name") != string::npos) {
-			config->name = line.substr(line.find('=') + 1);
-
-			if(config->name.size() > 16) {
-				cerr << "[WARNING] rfclient.name value is too long."
-					 << endl << "It will be truncated up to 16 characters." << endl;
-				config->name.resize(16);
-			}
-		}
-
-		if(line.find("rfclient.file_of_matlab") != string::npos) {
-			string fom = line.substr(line.find('=') + 1);
-
-			if(fom.size() > 16) {
-				cerr << "[WARNING] rfclient.file_of_matlab value is too long."
-					 << endl << "It will be truncated up to 16 characters." << endl;
-				fom.resize(16);
-			}
-			char *str = new char[fom.length() + 1];
-			strcpy(str, fom.c_str());
-			config->file_of_matlab = str;
-		}
-
-		if(line.find("rfclient.RULE_AMOUNT") != string::npos){
-			config->RULE_AMOUNT = atoi(line.substr(line.find('=') + 1).c_str());
-		}
-
-		if(line.find("rfclient.RULE_LENGTH") != string::npos){
-			config->RULE_LENGTH = atoi(line.substr(line.find('=') + 1).c_str());
-		}
-
-		if(line.find("rfclient.BACK_AMOUNT") != string::npos){
-			config->BACK_AMOUNT = atoi(line.substr(line.find('=') + 1).c_str());
-		}
-
-		if(line.find("rfclient.BACK_LENGTH") != string::npos){
-			config->BACK_LENGTH = atoi(line.substr(line.find('=') + 1).c_str());
-		}
-	}
-
-	configFile.close();
-	return 0;
-}
 
 MainAlgWorker::MainAlgWorker()
 	: mStatisticsTimer(this)
@@ -123,24 +57,13 @@ void MainAlgWorker::init(){
 
 	cout << "Initialization config file..." << endl;
 
-	if(!initConfig(&rcconfig)) {
-		cout << rcconfig.file_of_matlab << endl;
-		cout << rcconfig.name << endl;
-		cout << rcconfig.BACK_AMOUNT << endl;
-		cout << rcconfig.BACK_LENGTH << endl;
-		cout << rcconfig.RULE_AMOUNT << endl;
-		cout << rcconfig.RULE_LENGTH << endl;
-
-		cout << "...successful" << endl;
-	} else {
-		cerr << "...bad" << endl;
-		rcconfig.name = "Robofootball";
-		rcconfig.file_of_matlab = "main";
-		rcconfig.RULE_AMOUNT = 5;
-		rcconfig.RULE_LENGTH = 7;
-		rcconfig.BACK_AMOUNT = 10;
-		rcconfig.BACK_LENGTH = 8;
-	}
+	cerr << "...bad" << endl;
+	rcconfig.name = "Robofootball";
+	rcconfig.file_of_matlab = "main";
+	rcconfig.RULE_AMOUNT = 5;
+	rcconfig.RULE_LENGTH = 7;
+	rcconfig.BACK_AMOUNT = 10;
+	rcconfig.BACK_LENGTH = 8;
 
 	MlData mtl(rcconfig);
 	fmldata = mtl;
